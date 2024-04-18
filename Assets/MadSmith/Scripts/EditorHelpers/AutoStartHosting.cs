9666d0589c2e0abf04e7f3e8175051ea68249830
Vitor Soares Silva <vitrola.vitor@gmail.com>
2024-04-18 08:09:01 -0300

    [ADD] Auto Start host prefab

---
+++ Assets/MadSmith/Scripts/EditorHelpers/AutoStartHosting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Mirror;
+using UnityEngine;
+
+namespace MadSmith.Scripts.EditorHelpers
+{
+    public class AutoStartHosting : MonoBehaviour
+    {
+        [SerializeField] private bool hostOnStart = true;
+
+        private void Start()
+        {
+            if (hostOnStart)
+            {
+                NetworkManager.singleton.StartHost();
+            }
+        }
+    }
+}
